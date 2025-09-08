cac3c099d0310c207054c65f3f3eb08b5b8faf32
Juan Esteban Cárdenas Cardona <juanesteban1417@gmail.com>
2025-09-07 19:16:38 -0500

    Update exersice

---
+++ Taller_1_POO.BConcepts/Taller_1_POO.BConcepts/Time.cs
@@ -0,0 +1,56 @@
+﻿namespace Taller_1_POO.BConcepts.Core;
+
+public class Time
+{
+
+    //Fields
+    private int _hour;
+    private int _millisecond;
+    private int _minute;
+    private int _second;
+
+
+
+    //Public Properties
+    public int Hour
+    {
+        get => _hour;
+        set
+        {
+            _hour = value;
+        }
+    }
+
+    public int Millisecond
+    {
+        get => _millisecond;
+        set
+        {
+            _millisecond = value;
+        }
+    }
+
+    public int Minute
+    {
+        get => _minute;
+        set
+        {
+            _minute = value;
+        }
+    }
+
+    public int Second
+    {
+        get => _second;
+        set
+        {
+            _second = value;
+        }
+    }
+
+
+
+
+
+
+}
